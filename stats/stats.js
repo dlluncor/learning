a35bbb6175983d878504787a82957b7f24947780
dlluncor <david.lluncor@gmail.com>
2013-03-11 00:49:30 -0700

    Second level descriptions of the set of samples.

--- stats/stats.js
+++ stats/stats.js
@@ -14,43 +14,76 @@ MyMath.mean = function(arr) {
   return sum / arr.length;
 };
 
-var stats = {};
+// Computes the sample standard deviation.
+MyMath.sampleStdev = function(samples, n) {
+  var mu = MyMath.mean(samples);
+  var sumSq = 0;
+  samples.forEach(function(sample) {
+    sumSq += Math.pow(sample - mu, 2);
+  });
+  return sumSq / (n - 1);
+};
 
-stats = function(distrib) {
+// Is the mean within the range of these two values?
+MyMath.isMeanWithinRange = function(lower, upper, meanVal) {
+  if (meanVal < lower) {
+    return false;
+  } else if (meanVal > upper) {
+    return false;
+  }
+  return true;
+};
+
+var Stats = {};
+
+Stats = function(distrib) {
   this.distrib = distrib;
 };
 
 // Take a sample n times and return the mean.
-stats.prototype.sampleMean = function(n) {
+Stats.prototype.sampleMeanInfo = function(n) {
   if (n == 0) {
     throw Error('This is a problem.');
   }
   var N = this.distrib.length;
 
   var sum = 0;
+  var samples = []; // samples taken during estimate.
   for (var i = 0; i < n; i++) {
     var index = Math.floor(Math.random() * N);
     if (index == N) {
       index = N - 1;
     }
     sum += this.distrib[index];
+    samples.push(this.distrib[index]);
   }
-  return sum / n;
+  var sampleMeanInfo = {
+    'sampleMean': sum / n,
+    'n': n,
+    'samples': samples
+  };
+  return sampleMeanInfo;
 };
 
 // Generate a sample distribution for a given number of trials
 // and a certain number of samples.
-stats.prototype.sampleDistribution = function(trials, n) {
-  var means = [];
+Stats.prototype.sampleDistributionInfo = function(trials, n) {
+  var meanInfos = [];
 
   for (var i = 0; i < trials; i++) {
-    means.push(this.sampleMean(n));
+    meanInfos.push(this.sampleMeanInfo(n));
   }
-  return means;
+  return meanInfos;
 }
 
+Stats.getDistributions = function(sampleMeanInfos) {
+  return sampleMeanInfos.map(function(sampleMeanInfo) {
+    return sampleMeanInfo.sampleMean;
+  });
+};
+
 // First level characteristics of a list of numbers.
-stats.prototype.firstLevelStats = function(distrib) {
+Stats.firstLevelStats = function(distrib) {
   var n = distrib.length;
   // Mean.
   var mean = MyMath.mean(distrib);
@@ -83,11 +116,45 @@ stats.prototype.firstLevelStats = function(distrib) {
 
 };
 
-var shower = function() {
+// Second level characteristics like likelihood the actual
+// mean falls within the bounds estimated by each of the numbers
+// in the distribution.
+Stats.secondLevelStats = function(sampleMeanInfos, actualMean) {
+  // Need samples taken, n, and mean of samples.
+
+  // Number of times we estimated the mean was within our range
+  // correctly.
+  var numTimesInRange = 0;
+
+  sampleMeanInfos.forEach(function(sampleMeanInfo) {
+    var n = sampleMeanInfo.n;
+    var s = MyMath.sampleStdev(sampleMeanInfo.samples, n);
+    var stdevOfSamplingDistrib = s / Math.sqrt(n);
+    var mu_sampleMean = sampleMeanInfo.sampleMean;
+
+    // Compute 95% confidence your estimate falls in this range.
+    var oneSideRange = 1.96 * stdevOfSamplingDistrib;
+    var lowerRange = mu_sampleMean - oneSideRange;
+    var upperRange = mu_sampleMean + oneSideRange;
+    var text = mu_sampleMean + ' [ ' + lowerRange + ',' + upperRange + ']';
+    var isMeanInRange = MyMath.isMeanWithinRange(lowerRange, upperRange, actualMean);
+    if (isMeanInRange) {
+      text += ' YES';
+      numTimesInRange += 1;
+    } else {
+      text += ' NO';
+    }
+    //print (text);
+  });
+
+  print ('Prob predicted correct: ' + (numTimesInRange / sampleMeanInfos.length) * 100);
+}
+
+var Shower = function() {
 };
 
 // Prints a distribution of numbers.
-shower.prototype.show = function(distrib) {
+Shower.prototype.show = function(distrib) {
   // Get list of numbers with their probability of occuring.
   var probs = {};
   // Compute an object with the number and its probability of occuring.
@@ -161,15 +228,24 @@ ctrl.runStats = function() {
   ctrl.clearEls();
   var myInput = ctrl.getInput();
 
-  var origDistrib = ctrl.getOriginalDistribution();   
-  var show0 = new shower();
+  print('Population parameters:');
+  var origDistrib = ctrl.getOriginalDistribution();
+  Stats.firstLevelStats(origDistrib);
+  var origMean = MyMath.mean(origDistrib);
+
+  var show0 = new Shower();
   show0.show(origDistrib);
 
-  var woah = new stats(origDistrib);
-  var distrib = woah.sampleDistribution(myInput.trials, myInput.n);
+  print('Sampling parameters:');
+
+  var woah = new Stats(origDistrib);
+  var distribInfo = woah.sampleDistributionInfo(myInput.trials, myInput.n);
+  var distrib = Stats.getDistributions(distribInfo);
   show0.show(distrib);
   // Get first level characteristics.
-  woah.firstLevelStats(distrib);
+  Stats.firstLevelStats(distrib);
+  // Get z stat inferential characteristics.
+  Stats.secondLevelStats(distribInfo, origMean);
 };
 
 $(document).ready(ctrl.init);
