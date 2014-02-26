defe0efde587d078c8e68dc594d715f1533ad830
dlluncor <david.lluncor@gmail.com>
2014-02-26 01:06:09 -0800

    Iron cross is more or less implemented as a strategy. Still need to check on all the odds and whether I do win and win stay correctly.

--- cplus/tutorial/lessons.cc
+++ cplus/tutorial/lessons.cc
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <stdlib.h>
+#include <assert.h>
 #include "lessons.h"
 #include <map>
 #include <sstream>
 using namespace std;
+
+// Stack traces.
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+//#include <unistd.h>
 
 int Die::Roll() {
   // Roll a value 1 to 6.
@@ -36,11 +43,11 @@ float ScaleHardHit(float value, int die) {
   }
   return -1; // Should never get here.
 }
+*/
 
 bool InField(float sum) {
   return sum == 2 || sum == 3 || sum == 4 || sum == 9 || sum == 10 || sum == 11 || sum == 12;
 }
-*/
 
 // Bet has the following components:
 // Name: "yo"
@@ -58,26 +65,19 @@ bool InField(float sum) {
 // Utils.
 float RolledToOdds(int on_num) {
   if (on_num == 6 || on_num == 8) {
-    return 7 / 6;
+    return 7 / 6.0;
   }
   if (on_num ==  5 || on_num == 9) {
-    return 7 / 5;
+    return 7 / 5.0;
   }
   if (on_num = 4 || on_num == 10) {
-    return 9 / 5;
+    return 9 / 5.0;
   }
 }
 
 // Win predicates.
 
 // Three types of bets need to organize them!
-
-string HardTwoNext(int die0, int die1, bool is_on) {
-  if ((die0 + die1) == 2) {
-    return "win"; // always give back money for middle bets...I think.
-  }
-  return "lose";
-}
 
 string PassNext(int on_num, int die0, int die1, bool is_on) {
   int sum = die0 + die1;
@@ -93,12 +93,67 @@ string PassNext(int on_num, int die0, int die1, bool is_on) {
   return "";
 }
 
-float PassOdds(int on_num) {
+string HardTwoNext(int die0, int die1, bool is_on) {
+  if ((die0 + die1) == 2) {
+    return "win"; // always give back money for middle bets...I think.
+  }
+  return "lose";
+}
+
+string FieldNext(int die0, int die1, bool is_on) {
+  if (InField(die0 + die1)) {
+    return "win";
+  }
+  return "lose";
+}
+
+string FiveNext(int on_num, int die0, int die1, bool is_on) {
+  if ((die0 + die1) == 5) {
+    return "win_stay";  // You win but can't remove the bet itself.
+  }
+  if (PassNext(on_num, die0, die1, is_on) == "lose") {
+    return "lose"; // If we dont lose, we stay around.
+  }
+  return "";  // We didn't win, and we didn't lose, stick around.
+}
+
+string SixNext(int on_num, int die0, int die1, bool is_on) {
+  if ((die0 + die1) == 6) {
+    return "win_stay";  // You win but can't remove the bet itself.
+  }
+  if (PassNext(on_num, die0, die1, is_on) == "lose") {
+    return "lose"; // If we dont lose, we stay around.
+  }
+  return "";  // We didn't win, and we didn't lose, stick around.
+}
+
+string EightNext(int on_num, int die0, int die1, bool is_on) {
+  if ((die0 + die1) == 8) {
+    return "win_stay";  // You win but can't remove the bet itself.
+  }
+  if (PassNext(on_num, die0, die1, is_on) == "lose") {
+    return "lose"; // If we dont lose, we stay around.
+  }
+  return "";  // We didn't win, and we didn't lose, stick around.
+}
+
+float PassOdds(int on_num, int sum) {
   return RolledToOdds(on_num);
 }
 
-bool IsPassBet(string bet_name) {
-  return bet_name == "pass";  // TODO(dlluncor): Update.
+float FieldOdds(int on_num, int sum) {
+  if (sum == 2 || sum == 12) {
+    return 2.0;
+  }
+  return 1.0;
+}
+
+bool Craps::IsPassBet(string bet_name) {
+  return name_to_next_pass.find(bet_name) != name_to_next_pass.end();
+}
+
+bool Craps::IsPassBetOdds(string bet_name) {
+  return name_to_odds_pass.find(bet_name) != name_to_odds_pass.end();
 }
 
 
@@ -114,6 +169,7 @@ void Craps::Roll() {
   int val0 = die0->Roll();
   int val1 = die1->Roll();
   cout << "Rolled " << val0 << ", " << val1 << "\n";
+  vector<string> remove_bets;  // list of keys for bets to remove if win or lose.
   for (auto& bet_tup : bets) {
     BetInfo bet_info;
     auto& bet = bet_tup.second;
@@ -135,8 +191,8 @@ void Craps::Roll() {
     // Determine what to do with money.
     if (next_state == "win" || next_state == "win_stay") {
       float payout_multiplier;
-      if (IsPassBet(name)) {
-        payout_multiplier = name_to_odds_pass[name](on_num);
+      if (IsPassBetOdds(name)) {
+        payout_multiplier = name_to_odds_pass[name](on_num, val0 + val1);
       } else {
         payout_multiplier = name_to_odds[name];
       }
@@ -145,7 +201,7 @@ void Craps::Roll() {
         // again.
         next_value = (payout_multiplier*bet.value) + bet.value;
         Pay(next_value, bet.player);
-        bets.erase(bet_key);
+        remove_bets.push_back(bet_key);
       } else {
         // Pay the player their winnings but don't remove their bet.
         next_value = (payout_multiplier*bet.value);
@@ -154,7 +210,8 @@ void Craps::Roll() {
     } else if (next_state == "lose") {
       // Remove the bet!
       next_value = 0;
-      bets.erase(bet_key);
+      remove_bets.push_back(bet_key);
+      //bets.erase(bet_key);
     } else if (next_state != "") {
       // Then we need to transition the money to another bet (COME) bet.
       bet.name = next_state;  // TODO: Will this actually work?? Or a bug??
@@ -167,6 +224,9 @@ void Craps::Roll() {
     bet_info.next_state = next_state;
     bet_info.next_value = next_value;
     last_changed_bets[bet.player].push_back(bet_info);
+  }
+  for (auto& bet_name: remove_bets) {
+    bets.erase(bet_name);
   }
 
   string next_button_state = PassNext(on_num, val0, val1, is_on);
@@ -186,7 +246,7 @@ void Craps::Roll() {
 }
 
 void Craps::Pay(float amount, PlayerId player) {
-  cout << "Paying player " << player << ": $" << amount << "\n";
+  //cout << "Paying player " << player << ": $" << amount << "\n";
   bankrolls[player] += amount;
 }
 
@@ -194,19 +254,27 @@ void Craps::Init() {
   // Odds predicates (for payout). I think they get more complicated than this....
   // TODO(dlluncor): Fill in more next states and odds.
   name_to_odds = {
-    {"hardTwo", 30.0}
+    {"hardTwo", 30.0},
+    {"Five", 7/5.0},
+    {"Six", 7/6.0},
+    {"Eight", 7/6.0}
   };
   name_to_odds_pass = {
     {"pass", *PassOdds},
+    {"Field", *FieldOdds},
   };
 
   // Next state predicates (only ones that matter), they determine whether you win as well.
   name_to_next = {
-    {"hardTwo", *HardTwoNext}
+    {"hardTwo", *HardTwoNext},
+    {"Field", *FieldNext}
   };
 
   name_to_next_pass = {
     {"pass", *PassNext},
+    {"Five", *FiveNext},
+    {"Six", *SixNext},
+    {"Eight", *EightNext}
   };
 }
 
@@ -235,8 +303,9 @@ void Craps::InspectState() {
   }
   printf("Players:\n");
   for (auto& paid_tup: paid) {
-    auto& player = paid_tup.first;
-    printf("  Player %d put in $%.1f and has %.1f\n", player, paid_tup.second, bankrolls[player]);
+    auto& player_id = paid_tup.first;
+    Player* player = cur_players[player_id];
+    printf("  Player %s put in $%.1f and has $%.1f\n", player->str().c_str(), paid_tup.second, bankrolls[player_id]);
   }
 }
 
@@ -254,10 +323,10 @@ void Craps::Decide(PlayerId player) {
 
 PlayerId Craps::Register(Player* player) {
   // This is the only place where we "initialize a player".
+  next_player_id++;
   paid[next_player_id] = 0;
   bankrolls[next_player_id] = 0;
   last_changed_bets[next_player_id] = {};
-  next_player_id++;
   cur_players[next_player_id] = player;
   return next_player_id;
 }
@@ -281,15 +350,22 @@ void Craps::AddBet(Bet bet) {
   char* str;
   stringstream key;
   key << bet.name << "**" << bet.player;
+  printf("Player %d added bet: %s\n", bet.player, bet.name.c_str());
   bets[key.str()] = bet;
   // Deduct from player's bankroll.
   bankrolls[bet.player] -= bet.value;
 }
 
 void Craps::RunStrategies() {
+  GameState game_state;
+  game_state.is_on = is_on;
+  game_state.on_num = on_num;
   for (auto& player_kv: cur_players) {
     Player* player = player_kv.second;
-    player->NextActions();
+    PlayerId player_id = player_kv.first;
+    PlayerDecision decision(game_state, last_changed_bets[player_id],
+      bankrolls[player_id]);
+    player->NextActions(decision);
     // Need to export the game state and set of bets relevant to this player for them
     // to decide what to do next.
   }
@@ -327,6 +403,63 @@ void Lessons() {
 
 }
 
+// List of strategies. They all take decision info for a player and then make the appropriate
+// named bet with the right amount of dollars.
+
+vector<Bet> PassOnly(const PlayerDecision& decision) {
+  vector<Bet> bets;
+  bool is_off = !decision.game_state.is_on;
+  if (is_off) {
+    // When the die roll is off, we want to simply put 10 on the pass line. Our money
+    // is not still on the table.
+    Bet bet("pass", 10.0);
+    bets.push_back(bet);
+  }
+  return bets;
+}
+
+vector<Bet> IronCross(const PlayerDecision& decision) {
+  float default_bet = 10.0;
+  vector<Bet> bets;
+  map<BetName, bool> cur_bets_map;
+  for (auto& bet_info: decision.bet_infos) {
+    if (bet_info.next_value > 0) {
+       cur_bets_map[bet_info.next_state] = true;
+    }
+  }
+
+  bool is_off = !decision.game_state.is_on;
+  int on_num = decision.game_state.on_num;
+  if (is_off) {
+    // When the die roll is off, we want to simply put 10 on the pass line. Our money
+    // is not still on the table.
+    Bet bet("pass", default_bet);
+    bets.push_back(bet);
+  } else {
+    // We are on, we need to look at the button and our bets.
+    if (on_num != 5 && (cur_bets_map.find("Five") == cur_bets_map.end())) {
+      // Make a bet on 5 if we don't have a bet down already.
+      Bet bet("Five", default_bet);
+      bets.push_back(bet);
+    }
+    if (on_num != 6 && (cur_bets_map.find("Six") == cur_bets_map.end())) {
+      // Make a bet on 6 if we don't have a bet down already.
+      Bet bet("Six", default_bet);
+      bets.push_back(bet);
+    }
+    if (on_num != 8 && (cur_bets_map.find("Eight") == cur_bets_map.end())) {
+      // Make a bet on 8 if we don't have a bet down already.
+      Bet bet("Eight", default_bet);
+      bets.push_back(bet);
+    }
+    if(cur_bets_map.find("Field") == cur_bets_map.end()) {
+      // Make a bet on the field if we don't have a bet down already.
+      Bet bet("Field", default_bet);
+      bets.push_back(bet); 
+    }
+  }
+  return bets;
+}
 
 Player::Player(Game* game_, string name_, string strategy_) {
   game = game_;
@@ -334,12 +467,40 @@ Player::Player(Game* game_, string name_, string strategy_) {
   strategy = strategy_;
 }
 
-void Player::NextActions() {
-
+void Player::NextActions(
+  const PlayerDecision& decision) {
+  vector<Bet> bets;
+  if (strategy == "iron_cross") {
+    bets = IronCross(decision);
+  } else if (strategy == "pass_only") {
+    bets = PassOnly(decision);
+  } else {
+    printf("Unrecognized strategy %s\n", strategy.c_str());
+    assert(0 == 1);
+  }
+  // Make sure the player has enough money for the bets, if not pay for more.
+  float total = 0;
+  for (auto& bet : bets) {
+    total += bet.value;
+  }
+  if (decision.amount < total) {
+    game->Buyin(id, (total - decision.amount) + 10);
+  }
+  // Add bets that your strategy told you to do.
+  for (auto& bet : bets) {
+    bet.player = id;  // Need to set which player this bet belongs to.
+    game->AddBet(bet);
+  }
 }
 
 void Player::set_id(PlayerId id_) {
   id = id_;
+}
+
+string Player::str() {
+  string s;
+  s += name + " (" + strategy + ")";
+  return s;
 }
 
 /*
@@ -350,16 +511,24 @@ void Player::set_id(PlayerId id_) {
 
 void TestStrategyMain() {
   Craps craps;
-  Player player0(&craps, "Joe", "iron cross");
-  PlayerId id = craps.Register(&player0);
-  player0.set_id(id);
-  craps.RunStrategies();
+  Player player0(&craps, "Ben", "iron_cross");
+  player0.set_id(craps.Register(&player0));
+  Player player1(&craps, "David", "pass_only");
+  player1.set_id(craps.Register(&player1));
+  for (int i = 0; i < 100; i++) {
+    craps.Roll();
+    craps.RunStrategies();
+  }
+  craps.InspectState();
 }
 
 void CommandLineMain() {
   Craps craps;
-  craps.Buyin(0, 200);
-  craps.AddBet(Bet{"pass", 0, 10.0});
+  PlayerId id = 0;
+  craps.Buyin(id, 200);
+  Bet bet("pass", 10.0);
+  bet.player = id;
+  craps.AddBet(bet);
   string resp;
   while (true) {
     cout << "Type r (roll): ";
--- cplus/tutorial/lessons.h
+++ cplus/tutorial/lessons.h
@@ -6,6 +6,10 @@
 #include <vector>
 #include <map>
 
+#define TRACE_MSG fprintf(stderr, __FUNCTION__     \
+                          "() [%s:%d] here I am\n", \
+                          __FILE__, __LINE__)
+
 using namespace std;
 
 class Die {
@@ -28,12 +32,15 @@ struct Bet {
   BetName name;
   PlayerId player;
   float value;
+
+  Bet(BetName name_, float value_): name(name_), value(value_){};
+  Bet(){};
 };
 
 typedef string (*BetNext)(int die0, int die1, bool is_on);
 // Bets that depend on the "ON" position.
 typedef string (*BetNextPass)(int on_num, int die0, int die1, bool is_on);
-typedef float (*BetOddsPass) (int on_num);
+typedef float (*BetOddsPass) (int on_num, int sum);
 
 class Game {
   public:
@@ -41,15 +48,34 @@ class Game {
       //cout << "Destroy games\n";
     };
     void Name(){ cout << "I am a game" << "\n"; };
+    virtual void AddBet(Bet bet) = 0;
+    virtual void Buyin(PlayerId id, float amount) = 0;
   private:
     string game_;
+};
+
+// Visible state of the game needed for a player to decide how to bet next.
+struct GameState {
+  bool is_on;
+  int on_num;
+};
+
+// Info needed for a player to make a decision.
+struct PlayerDecision {
+  GameState game_state;
+  vector<BetInfo> bet_infos;
+  float amount;  // amount this player has.
+  PlayerDecision(const GameState& game_state_, const vector<BetInfo>& bet_infos_,
+    float amount_):
+    game_state(game_state_), bet_infos(bet_infos_), amount(amount_){};
 };
 
 class Player {
   public:
     Player(Game* game, string name, string strategy);
     void set_id(PlayerId id);
-    void NextActions();
+    void NextActions(const PlayerDecision& decision);
+    string str();
   private:
     Game* game;
     string name;
@@ -92,6 +118,8 @@ class Craps : public Game {
   map<BetName, BetNextPass> name_to_next_pass;
   map<BetName, float> name_to_odds;
   map<BetName, BetOddsPass> name_to_odds_pass;
+  bool IsPassBet(BetName name);
+  bool IsPassBetOdds(BetName name); 
 
   // To help each player decide what bet to make next.
   map<PlayerId, vector<BetInfo>> last_changed_bets;
